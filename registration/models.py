f8294ca571a311dc877965061371ca4fcd6aadad
Ben <bwilliams@AUX-ad140c.local>
2014-07-08 09:34:26 -0400

    initial commit, added player model

---
+++ registration/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+
+class Player(models.Model):
+    GENDER_CHOICES = (
+        ('f', 'female'),
+        ('m', 'male'),
+    )
+
+    SKILL_CHOICES = (
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+        (5, '5'),
+    )
+
+    BUY_CHOICES = (
+        ('shirt', 'shirt'),
+        ('frisbee', 'frisbee')
+    )
+
+    SHIRT_SIZE_CHOICES = (
+        ('s', 'small'),
+        ('m','medium'),
+        ('l','large'),
+    )
+
+    POSITION_CHOICES = (
+        ('ic', 'in cutter'),
+        ('oc', 'out cutter'),
+        ('sdh', 'short distance handler'),
+        ('ldh', 'long distance handler'),
+    )
+
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField(max_length=30)
+    phone_number = models.CharField(max_length=30)
+    gender = models.CharField(choices=GENDER_CHOICES,max_length=1)
+    skill = models.IntegerField(choices=SKILL_CHOICES)
+    buy_options = models.CharField(choices=BUY_CHOICES,max_length=10)
+    shirt_size = models.CharField(choices=SHIRT_SIZE_CHOICES,max_length=1)
+    position = models.CharField(choices=POSITION_CHOICES,max_length=3)
+
+    def __unicode__(self):
+        return self.first_name + " " + self.last_name
